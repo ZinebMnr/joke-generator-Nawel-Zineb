c3cf57c5061bf2fe5374913599333081204b3cdf
Zineb <zineb12manar@gmail.com>
2025-09-16 11:21:51 +0200

    Update joke_generator.py

--- joke_generator.py
+++ joke_generator.py
@@ -2,9 +2,18 @@ import random
 
 # Initial joke collection
 jokes = [
-    "Pourquoi les développeurs préfèrent le mode sombre ? Parce que la lumière attire les bugs !",
-    "Combien de développeurs faut-il pour changer une ampoule ? Aucun, c'est un problème hardware.",
-    "Pourquoi les développeurs Java portent des lunettes ? Parce qu'ils ne peuvent pas C# !"
+ "Pourquoi les canards ont-ils autant de plumes ? Pour couvrir leur derrière.",
+"Que dit une porte quand on la pousse trop fort ? Arrête de me claquer !",
+"Pourquoi les maths adorent la forêt ? Parce qu’elles ont plein de racines.",
+"Qu’est-ce qu’un oignon dit à un autre oignon ? On se retrouve au pot !",
+"Pourquoi les vélos ne tiennent-ils pas debout tout seuls ? Parce qu’ils sont trop fatigués.",
+"Que dit une poule qui a trouvé un trèfle à quatre feuilles ? C’est mon jour de chance !",
+"Pourquoi les fantômes aiment-ils les ascenseurs ? Parce que ça les élève.",
+"Que fait un mouton quand il a froid ? Il met un pull-over.",
+"Pourquoi les horloges sont-elles toujours stressées ? Parce qu’elles sont pressées.",
+"Que dit une abeille quand elle rentre chez elle ? Honey, I’m home !",
+"Pourquoi les bananes vont-elles jamais à l’hôpital ? Parce qu’elles ont la peau dure."
+
 ]
 
 def print_random_joke():
